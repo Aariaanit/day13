fe5b7e3576d7ca30849f4a1310da26f0ab2c0d96
Aariaanit <arianittershnjaku@gmail.com>
2024-02-05 11:48:21 +0100

    05.02.2024

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
 
 export default function App() {
+  registerNNPushToken(18835, 'FlqGO7s5Cm2H8b0E9JyGrt');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
